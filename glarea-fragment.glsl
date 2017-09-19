b223c4fbb69cd1704db0704745dfd27f41768259
Lapshin Dmitry (LDVSOFT) <lapshin.dv@mail.ru>
2017-09-19 12:27:47 +0300

    Adjusted iterations & coloring

--- glarea-fragment.glsl
+++ glarea-fragment.glsl
@@ -10,12 +10,24 @@ out vec4 outputColor;
 
 void main() {
 	highp vec2 c = planePosition;
+	vec4 insideColor = vec4(0, 0, 0, 1);
+
+	/* skip main cardiod */ {
+		float phi = atan(c.y, c.x - .25);
+		float rho_c = .5 - cos(phi) / 2;
+		float rho = length(c - vec2(.25, 0));
+		if (rho < rho_c) {
+			outputColor = insideColor;
+			return;
+		}
+	}
+
 	highp vec2 z = vec2(0, 0);
 	int it;
 	for (it = 0; it < iterations; ++it) {
 		z = cx_mul(z, z) + c;
 		if (length(z) > 4) {
-			float h = it % 360;
+			float h = mod(log(it) * 20, 360);
 			if (h <= 60) {
 				outputColor = vec4(1, h / 60, 0, 1);
 			} else if (h <= 120) {
@@ -32,6 +44,5 @@ void main() {
 			return;
 		}
 	}
-	vec2 t = (c + 1) / 2;
-	outputColor = vec4(0, 0, 0, 1);
+	outputColor = insideColor;
 }
